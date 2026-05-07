7f02a3d8e8fa20fba03e72dc6598a14e18f2da20
Bugra020 <bugrakaya020@gmail.com>
2026-05-08 00:24:32 +0300

    added leaderboard to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import api from "../services/api"
 function Dashboard() {
 
     const [user, setUser] = useState(null)
+    const [leaderboard, setLeaderboard] = useState(null)
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState("")
 
@@ -12,15 +13,17 @@ function Dashboard() {
 
         const fetchUser = async () => {
             try {
-                const res = await api.get("/user/me")
-                setUser(res.data.data)
+                const userinfo_res = await api.get("/user/me")
+                setUser(userinfo_res.data.data)
+
+                const leaderboard_res = await api.get("/leaderboard")
+                setLeaderboard(leaderboard_res.data.data)
 
             } catch (err) {
-                setError("Failed to load user info")
+                setError("Failed to load dashboard")
 
             } finally {
                 setLoading(false)
-
             }
         }
 
@@ -36,20 +39,55 @@ function Dashboard() {
         return <h1>{error}</h1>
     }
 
-    let normalised_date = user.account_create_date.slice(0,10);
+    let normalised_date = user.account_create_date.slice(0, 10);
 
     return (
-        <div className="user-info-card">
-            <div className="user-info-title">
-                <div className="username">{user.username}</div>
-                <div className="id">{user.id}</div>
-                <div className="elo">{user.elo}</div>
+        <div>
+            <div className="user-info-card">
+                <div className="user-info-title">
+                    <div className="username">{user.username}</div>
+                    <div className="id">{user.id}</div>
+                    <div className="elo">{user.elo}</div>
+                </div>
+                <hr></hr>
+                <div>Total matches: {user.total_matches}</div>
+                <div>Email: {user.email}</div>
+                <div>Account date: {normalised_date}</div>
             </div>
-            <hr></hr>
-            <div>Total matches: {user.total_matches}</div>
-            <div>Email: {user.email}</div>
-            <div>Account date: {normalised_date}</div>
+            <div className="leaderboard-container">
+                <h2 className="leaderboard-title">Leaderboard</h2>
+
+                <table className="leaderboard-table">
+                    <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Username</th>
+                            <th>ELO</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {leaderboard.map((user, index) => (
+                            <tr key={user.username}>
+                                <td>#{index + 1}</td>
+                                <td className={
+                                    index === 0
+                                        ? "first"
+                                        : index === 1
+                                            ? "second"
+                                            : index === 2
+                                                ? "third"
+                                                : ""
+                                }>{user.username}</td>
+                                <td>{user.elo}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
         </div>
+
     )
 }
 
